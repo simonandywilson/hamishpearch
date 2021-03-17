8c86703739538d7809c28df3572efae217077aa2
Simon Wilson <contact@simonwilson.design>
2021-03-17 18:55:47 +0000

    404 updated

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,19 +1,27 @@
 import React from "react"
-// import {Link} from "gatsby"
+import {Link} from "gatsby"
 import Ticker from "react-ticker";
 
-const mainStyle = {
-    height: "100vh",
+const main = {
+    height: "calc(100vh - 15px)",
     width: "100vw",
     padding: "0",
     margin: "0",
+    paddingTop: "15px",
     overflow: "hidden"
 };
 
 const NotFound = () => {
   return (
-      <main style={mainStyle}>
-          {/* <Link to="/">Hamish Pearch</Link> */}
+      <main style={main}>
+          <Ticker direction={"toRight"} offset={"100%"}>
+              {() => (
+                  <>
+                      &nbsp;<Link to="/">Hamish Pearch</Link>&nbsp;
+                  </>
+              )}
+          </Ticker>
+
           <Ticker>
               {() => (
                   <>
